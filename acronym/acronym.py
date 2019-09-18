d0dc896b476e402affefd7a3545d5cbd2073ceec
Nicole Luo <bei.luo@chicagobooth.edu>
2019-09-18 16:39:03 +0000

    Solved Acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,14 @@
 def abbreviate(words):
-    pass
+    wrds = words.split()
+    #print(wrds)
+    hahah = []
+    count = len(wrds)
+    for i in range(count):
+       hahah.append(wrds[i][0]) 
+       i += 1
+    acronym = "".join(hahah)
+    print(acronym)
+        
+abbreviate("Portable Network Graphics")
+        
+    
